271acded9a7801c3f47128ba1ecd1aa892441b37
wangxiang6 <81965848@qq.com>
2020-04-15 22:52:44 +0800

    eureka自我保护

---
+++ cloudproviderpayment8002/src/main/java/com/wang/springcloud/PaymentApplication.java
@@ -0,0 +1,17 @@
+package com.wang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class PaymentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class, args);
+    }
+}
---
+++ cloudproviderpayment8002/src/main/java/com/wang/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,29 @@
+package com.wang.springcloud.service.impl;
+
+import com.wang.springcloud.dao.PaymentDao;
+import com.wang.springcloud.entity.Payment;
+import com.wang.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 10:40
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService
+{
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment)
+    {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id)
+    {
+        return paymentDao.getPaymentById(id);
+    }
+}
